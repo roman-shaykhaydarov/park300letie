9f9e47607c03b6611280dc3544ba87688395ca20
Gleb Svechnikov <g.svechnikov@infoculture.ru>
2021-09-09 11:04:28 +0300

    Update styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,12 @@
 module.exports = function (config) {
   config.addPassthroughCopy('src/images')
   config.addPassthroughCopy('src/styles')
-
+  config.addPassthroughCopy({
+    'node_modules/normalize.css/normalize.css': 'styles/normalize.css',
+  })
+  config.addPassthroughCopy({
+    'node_modules/flexboxgrid/dist/flexboxgrid.css': 'styles/flexboxgrid.css',
+  })
   return {
     dir: {
       input: 'src',
